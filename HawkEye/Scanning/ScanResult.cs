6971655d3a91ac57298cb1532900008ad99e5a66
Kitt3120 <torben.schweren@gmail.com>
2022-03-05 19:15:06 +0100

    Added documentation for ScanResult and ScanStatus

--- HawkEye/Scanning/ScanResult.cs
+++ HawkEye/Scanning/ScanResult.cs
@@ -8,17 +8,54 @@ namespace HawkEye.Scanning
 {
     public class ScanResult
     {
+        /// <summary>
+        /// DateTime of when the scan started
+        /// </summary>
         public DateTime BeginOfScan { get; protected set; }
+
+        /// <summary>
+        /// DateTime of when the scan finished, failed or has been aborted
+        /// </summary>
         public DateTime EndOfScan { get; protected set; }
+
+        /// <summary>
+        /// Calculates the timespan between begin and end of scan.
+        /// If the scan has not started or finished yet, the timespan will be of 0 ticks.
+        /// </summary>
         public TimeSpan TimeSpan { get => BeginOfScan.Ticks == 0 || EndOfScan.Ticks == 0 ? new TimeSpan(0) : EndOfScan - BeginOfScan; }
-        public ScanStatus Status { get; protected set; }
+
+        /// <summary>
+        /// The target file
+        /// </summary>
         public string File { get; private set; }
+
+        /// <summary>
+        /// Information about the current state
+        /// </summary>
+        public ScanStatus Status { get; protected set; }
+
+        /// <summary>
+        /// The scanner used for scanning the target file
+        /// </summary>
         public Scanner Scanner { get; protected set; }
+
+        /// <summary>
+        /// Information gathered by the scanner
+        /// </summary>
         public string Content { get; protected set; }
+
+        /// <summary>
+        /// On failing, this is set to the encountered exception.
+        /// Otherwise this is null.
+        /// </summary>
         public Exception ThrownException { get; protected set; }
 
         //TODO: Add issuer property (User or issued through automated job)
 
+        /// <summary>
+        /// Creates a new ScanResult
+        /// </summary>
+        /// <param name="file">The target file</param>
         public ScanResult(string file)
         {
             File = file;
--- HawkEye/Scanning/ScanStatus.cs
+++ HawkEye/Scanning/ScanStatus.cs
@@ -8,6 +8,24 @@ namespace HawkEye.Scanning
 {
     public enum ScanStatus
     {
-        Pending, Running, Aborted, Failed
+        /// <summary>
+        /// Indicates that a Scan is still pending and awaiting future execution
+        /// </summary>
+        Pending,
+
+        /// <summary>
+        /// Indicates that a Scan is currently running
+        /// </summary>
+        Running,
+
+        /// <summary>
+        /// Indicates that a Scan was pending or running and has been aborted
+        /// </summary>
+        Aborted,
+
+        /// <summary>
+        /// Indicates that a Scan has run into an exception and did not finish successfully
+        /// </summary>
+        Failed
     }
 }
